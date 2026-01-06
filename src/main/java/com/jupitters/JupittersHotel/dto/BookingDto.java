56e82da73e6ab18146cfdd12529e583b0db01e1f
jupitters <sampaiobale@gmail.com>
2026-01-06 19:39:54 -0300

    room dto

--- src/main/java/com/jupitters/JupittersHotel/dto/BookingDto.java
+++ src/main/java/com/jupitters/JupittersHotel/dto/BookingDto.java
@@ -1,4 +1,10 @@
 package com.jupitters.JupittersHotel.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class BookingDto {
+
 }
--- src/main/java/com/jupitters/JupittersHotel/dto/RoomDto.java
+++ src/main/java/com/jupitters/JupittersHotel/dto/RoomDto.java
@@ -1,4 +1,18 @@
 package com.jupitters.JupittersHotel.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.jupitters.JupittersHotel.model.Booking;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class RoomDto {
+    private String roomType;
+    private BigDecimal roomPrice;
+    private String roomPhotoUrl;
+    private String roomDescription;
+    private List<Booking> bookings;
 }
