3b5f458c0731f912e7d261184d2f4e9c71c8f64e
jupitters <sampaiobale@gmail.com>
2026-01-06 11:19:32 -0300

    init

---
+++ src/main/java/com/jupitters/JupittersHotel/JupittersHotelApplication.java
@@ -0,0 +1,13 @@
+package com.jupitters.JupittersHotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JupittersHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JupittersHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jupitters/JupittersHotel/JupittersHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jupitters.JupittersHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JupittersHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
