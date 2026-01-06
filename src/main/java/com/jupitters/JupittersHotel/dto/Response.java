9d8578ca7d8a72e3bc088bc2b7f27e492785e7c4
jupitters <sampaiobale@gmail.com>
2026-01-06 19:50:30 -0300

    response

---
+++ src/main/java/com/jupitters/JupittersHotel/dto/Response.java
@@ -0,0 +1,24 @@
+package com.jupitters.JupittersHotel.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Response {
+    private int statusCode;
+    private String message;
+    private String token;
+    private String role;
+    private String expirationTime;
+    private String bookingConfirmationCode;
+    private UserDto user;
+    private RoomDto room;
+    private BookingDto booking;
+    private List<UserDto> userList;
+    private List<RoomDto> roomList;
+    private List<BookingDto> bookingList;
+}
