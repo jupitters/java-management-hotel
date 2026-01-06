c4f7e5ce51a142134903b39709e6f2e7266851b7
jupitters <sampaiobale@gmail.com>
2026-01-06 19:42:12 -0300

    booking dto

--- src/main/java/com/jupitters/JupittersHotel/dto/BookingDto.java
+++ src/main/java/com/jupitters/JupittersHotel/dto/BookingDto.java
@@ -1,10 +1,27 @@
 package com.jupitters.JupittersHotel.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.jupitters.JupittersHotel.model.Room;
+import com.jupitters.JupittersHotel.model.User;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.Min;
 import lombok.Data;
+
+import java.time.LocalDate;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class BookingDto {
-
+    private Long id;
+    private LocalDate checkInDate;
+    private LocalDate checkOutDate;
+    private int numOfAdults;
+    private int numOfChildren;
+    private int totalNumOfGuests;
+    private String bookingConfirmationCode;
+    private User user;
+    private Room room;
 }
