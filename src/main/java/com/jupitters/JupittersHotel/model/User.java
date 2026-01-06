9d3a7a94ef40e48f3ac42767198273d1615d68ae
jupitters <sampaiobale@gmail.com>
2026-01-06 19:03:13 -0300

    user model

--- src/main/java/com/jupitters/JupittersHotel/model/User.java
+++ src/main/java/com/jupitters/JupittersHotel/model/User.java
@@ -1,11 +1,27 @@
 package com.jupitters.JupittersHotel.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @Entity
 @Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Email is required")
+    @Column(unique = true)
+    private String email;
+    private String name;
+    private String phoneNumber;
+    private String password;
+    private String role;
+    private List<Booking> bookings = new ArrayList<>();
 }
