7dc60a9bbb05f85aedee27e8cc554dc1f95a158f
jupitters <sampaiobale@gmail.com>
2026-01-06 11:58:38 -0300

    user model

---
+++ src/main/java/com/jupitters/JupittersHotel/model/User.java
@@ -0,0 +1,11 @@
+package com.jupitters.JupittersHotel.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+}
