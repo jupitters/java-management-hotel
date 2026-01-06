f83a6611d89b83e58e9b9055f5ac255232d726cb
jupitters <sampaiobale@gmail.com>
2026-01-06 19:42:36 -0300

    booking dto

--- src/main/java/com/jupitters/JupittersHotel/dto/BookingDto.java
+++ src/main/java/com/jupitters/JupittersHotel/dto/BookingDto.java
@@ -22,6 +22,6 @@ public class BookingDto {
     private int numOfChildren;
     private int totalNumOfGuests;
     private String bookingConfirmationCode;
-    private User user;
-    private Room room;
+    private UserDto user;
+    private RoomDto room;
 }
