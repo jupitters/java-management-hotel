bac9d37129fe832716d23384ed52576af187a23b
jupitters <sampaiobale@gmail.com>
2026-01-06 19:30:34 -0300

    room model

--- src/main/java/com/jupitters/JupittersHotel/model/Room.java
+++ src/main/java/com/jupitters/JupittersHotel/model/Room.java
@@ -1,4 +1,25 @@
 package com.jupitters.JupittersHotel.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "rooms")
 public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String bookingConfirmationCode;
+    private String roomType;
+    private BigDecimal roomPrice;
+    private String roomPhotoUrl;
+    private String roomDescription;
+    private List<Booking> bookings = new ArrayList<>();
 }
