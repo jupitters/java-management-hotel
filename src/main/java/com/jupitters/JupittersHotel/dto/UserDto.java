7703167f5f6c1c8298e33a5e2c5cbd4f25cbddf5
jupitters <sampaiobale@gmail.com>
2026-01-06 19:36:53 -0300

    user dto

--- src/main/java/com/jupitters/JupittersHotel/dto/UserDto.java
+++ src/main/java/com/jupitters/JupittersHotel/dto/UserDto.java
@@ -1,15 +1,19 @@
 package com.jupitters.JupittersHotel.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.jupitters.JupittersHotel.model.Booking;
 import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserDto {
-    private String id;
+    private Long id;
     private String email;
     private String name;
     private String phoneNumber;
     private String role;
-    private String bookings;
+    private List<Booking> bookings = new ArrayList<>();
 }
