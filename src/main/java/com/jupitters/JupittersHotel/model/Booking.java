ecb90d9cdee137fa3fa136491d76d498688c7478
jupitters <sampaiobale@gmail.com>
2026-01-06 19:10:42 -0300

    booking model

--- src/main/java/com/jupitters/JupittersHotel/model/Booking.java
+++ src/main/java/com/jupitters/JupittersHotel/model/Booking.java
@@ -1,4 +1,23 @@
 package com.jupitters.JupittersHotel.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table(name = "bookings")
 public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate checkInDate;
+    private LocalDate checkOutDate;
+
+    private int numOfAdults;
+    private int numOfChildren;
+    private int totalNumOfGuests;
 }
