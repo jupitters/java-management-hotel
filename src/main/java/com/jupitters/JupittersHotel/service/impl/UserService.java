11d222460973c9f3135481c6b8520c85d55727a1
jupitters <sampaiobale@gmail.com>
2026-01-07 18:50:33 -0300

    user service

---
+++ src/main/java/com/jupitters/JupittersHotel/service/impl/UserService.java
@@ -0,0 +1,15 @@
+package com.jupitters.JupittersHotel.service.impl;
+
+import com.jupitters.JupittersHotel.dto.LoginRequest;
+import com.jupitters.JupittersHotel.dto.Response;
+import com.jupitters.JupittersHotel.model.User;
+
+public interface UserService {
+    Response register(User loginRequest);
+    Response login(LoginRequest loginRequest);
+    Response getAllUsers();
+    Response getUserBookingHistory(Long userId);
+    Response deleteUser(Long userId);
+    Response getUserById(Long userId);
+    Response getMyInfo(Long userId);
+}
