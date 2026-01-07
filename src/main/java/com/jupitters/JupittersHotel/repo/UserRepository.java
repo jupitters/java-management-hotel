f24243565e682ba76b907c3cb7ee6b405a245b23
jupitters <sampaiobale@gmail.com>
2026-01-07 11:25:01 -0300

    user repository

--- src/main/java/com/jupitters/JupittersHotel/repo/UserRepository.java
+++ src/main/java/com/jupitters/JupittersHotel/repo/UserRepository.java
@@ -3,5 +3,9 @@ package com.jupitters.JupittersHotel.repo;
 import com.jupitters.JupittersHotel.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<Long, User> {
+    boolean existsByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
