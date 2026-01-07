4b8998550275f2a988a7d56bc64d098f3277b2a1
jupitters <sampaiobale@gmail.com>
2026-01-07 18:10:43 -0300

    utils

--- src/main/java/com/jupitters/JupittersHotel/utils/Utils.java
+++ src/main/java/com/jupitters/JupittersHotel/utils/Utils.java
@@ -1,4 +1,18 @@
 package com.jupitters.JupittersHotel.utils;
 
+import java.security.SecureRandom;
+
 public class Utils {
+    private static final String ALPHANUMERIC_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private static final SecureRandom secureRandom = new SecureRandom();
+
+    public static String generateRandomAlphanumeric(int length) {
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            int randomIndex = secureRandom.nextInt(ALPHANUMERIC_STRING.length());
+            char randomChar = ALPHANUMERIC_STRING.charAt(randomIndex);
+            sb.append(randomChar);
+        }
+        return sb.toString();
+    }
 }
