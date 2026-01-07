1411cdcb0331d7129e8030f96d06a575a26dde9d
jupitters <sampaiobale@gmail.com>
2026-01-07 12:13:57 -0300

    jwt utils

---
+++ src/main/java/com/jupitters/JupittersHotel/utils/JWTUtils.java
@@ -0,0 +1,49 @@
+package com.jupitters.JupittersHotel.utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Date;
+import java.util.function.Function;
+
+@Service
+public class JWTUtils {
+    private static final long EXPIRATION_TIME = 1000 * 60 * 60 * 24 * 7; // 7 days
+
+    private final SecretKey key;
+
+    @Value("${auth.token.jwtSecret}")
+    String secretString;
+
+    public JWTUtils(){
+        byte[] keyBytes = Base64.getDecoder().decode(secretString.getBytes(StandardCharsets.UTF_8));
+        this.key = new SecretKeySpec(keyBytes, "HmacSHA256");
+    }
+
+    public String generateToken(UserDetails user){
+        return Jwts.builder()
+                .subject(user.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(key)
+                .compact();
+    }
+
+    public String extractUsername(String token){
+        return extractClaims(token, Claims::getSubject);
+    }
+
+    private <T> T extractClaims(String token, Function<Claims, T> claimsFunction) {
+        return claimsFunction.apply(Jwts.parser().verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload());
+    }
+}
