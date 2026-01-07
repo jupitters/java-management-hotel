5d1b7d8b0a37be303b82feb76082dec8d5167c27
jupitters <sampaiobale@gmail.com>
2026-01-07 11:52:29 -0300

    exception

--- src/main/java/com/jupitters/JupittersHotel/exception/ResourceNotFoundException.java
+++ src/main/java/com/jupitters/JupittersHotel/exception/ResourceNotFoundException.java
@@ -1,6 +1,6 @@
 package com.jupitters.JupittersHotel.exception;
 
-public class ResourceNotFoundException extends RuntimeException{
+public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String s) {
         super(s);
     }
--- src/main/java/com/jupitters/JupittersHotel/service/impl/CustomUserDetailsService.java
+++ src/main/java/com/jupitters/JupittersHotel/service/impl/CustomUserDetailsService.java
@@ -15,6 +15,6 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.findByEmail(username).orElseThrow(new ResourceNotFoundException("Username/Email not found!"));
+        return userRepository.findByEmail(username).orElseThrow(() -> new ResourceNotFoundException("Username/Email not found!"));
     }
 }
