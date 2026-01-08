ac5877b3a0827dfa3db7c165d127ee950cf5c186
jupitters <sampaiobale@gmail.com>
2026-01-08 12:15:39 -0300

    room service

--- src/main/java/com/jupitters/JupittersHotel/service/impl/RoomServiceImpl.java
+++ src/main/java/com/jupitters/JupittersHotel/service/impl/RoomServiceImpl.java
@@ -1,6 +1,53 @@
 package com.jupitters.JupittersHotel.service.impl;
 
+import com.jupitters.JupittersHotel.dto.Response;
 import com.jupitters.JupittersHotel.service.RoomService;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
 public class RoomServiceImpl implements RoomService {
+    @Override
+    public Response addNewRoom(MultipartFile photo, String roomType, BigDecimal roomPrice, String description) {
+        return null;
+    }
+
+    @Override
+    public List<String> getAllRoomTypes() {
+        return List.of();
+    }
+
+    @Override
+    public Response getAllRooms() {
+        return null;
+    }
+
+    @Override
+    public Response deleteRoom(Long roomId) {
+        return null;
+    }
+
+    @Override
+    public Response updateRoom(Long roomId, String roomType, BigDecimal roomprice, MultipartFile photo) {
+        return null;
+    }
+
+    @Override
+    public Response getRoomById(Long roomId) {
+        return null;
+    }
+
+    @Override
+    public Response getAvailableRoomsByDateAndType(LocalDate checkInDate, LocalDate checkOutDate, String roomType) {
+        return null;
+    }
+
+    @Override
+    public Response getAllAvailableRooms() {
+        return null;
+    }
 }
