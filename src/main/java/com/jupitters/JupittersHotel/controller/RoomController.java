c21f054bc4b4d2f7aaa0bce53a92259c4b1f7212
jupitters <sampaiobale@gmail.com>
2026-01-09 18:16:04 -0300

    room controller

--- src/main/java/com/jupitters/JupittersHotel/controller/RoomController.java
+++ src/main/java/com/jupitters/JupittersHotel/controller/RoomController.java
@@ -1,7 +1,12 @@
 package com.jupitters.JupittersHotel.controller;
 
+import com.jupitters.JupittersHotel.dto.Response;
+import com.jupitters.JupittersHotel.service.BookingService;
 import com.jupitters.JupittersHotel.service.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class RoomController {
     @Autowired
     private RoomService roomService;
+    @Autowired
+    private BookingService bookingService;
+
+    @GetMapping("/all")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<Response> getAllBookings(){
+        Response response = bookingService.getAllBookings();
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
 }
