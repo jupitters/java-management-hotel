2b766820bf85c8e0dd6b6b6f0a8a0d528340cf14
jupitters <sampaiobale@gmail.com>
2026-01-09 18:25:54 -0300

    bookings controller

--- src/main/java/com/jupitters/JupittersHotel/controller/BookingController.java
+++ src/main/java/com/jupitters/JupittersHotel/controller/BookingController.java
@@ -1,9 +1,27 @@
 package com.jupitters.JupittersHotel.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.jupitters.JupittersHotel.dto.Response;
+import com.jupitters.JupittersHotel.model.Booking;
+import com.jupitters.JupittersHotel.service.BookingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/bookings")
 public class BookingController {
+    @Autowired
+    private BookingService bookingService;
+
+    @PostMapping("/book-room/{roomId}/{userId}")
+    @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('USER')")
+    public ResponseEntity<Response> saveBookings(@PathVariable Long roomId,
+                                                 @PathVariable Long userId,
+                                                 @RequestBody Booking bookingRequest) {
+
+        Response response = bookingService.saveBooking(roomId, userId, bookingRequest);
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+
+    }
 }
