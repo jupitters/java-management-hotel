40ccd7d6f6baabfe10d5d722b7c0bac8c05faf75
jupitters <sampaiobale@gmail.com>
2026-01-09 12:43:20 -0300

    user controller

--- src/main/java/com/jupitters/JupittersHotel/controller/UserController.java
+++ src/main/java/com/jupitters/JupittersHotel/controller/UserController.java
@@ -1,9 +1,24 @@
 package com.jupitters.JupittersHotel.controller;
 
+import com.jupitters.JupittersHotel.dto.Response;
+import com.jupitters.JupittersHotel.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/all")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<Response> getAllUsers(){
+        Response response = userService.getAllUsers();
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
 }
