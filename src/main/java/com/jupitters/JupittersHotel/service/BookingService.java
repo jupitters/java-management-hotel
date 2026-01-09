3f1e311603a03216a14d5eb438d1b08d82b328f1
jupitters <sampaiobale@gmail.com>
2026-01-08 23:12:31 -0300

    booking service

--- src/main/java/com/jupitters/JupittersHotel/service/BookingService.java
+++ src/main/java/com/jupitters/JupittersHotel/service/BookingService.java
@@ -1,4 +1,11 @@
 package com.jupitters.JupittersHotel.service;
 
+import com.jupitters.JupittersHotel.dto.Response;
+import com.jupitters.JupittersHotel.model.Booking;
+
 public interface BookingService {
+    Response saveBooking(Long roomId, Long userId, Booking bookingRequest);
+    Response findBookingByConfirmationCode(String confirmationCode);
+    Response getAllBookings();
+    Response cancelBooking(Long bookingId);
 }
--- src/main/java/com/jupitters/JupittersHotel/service/impl/BookingServiceImpl.java
+++ src/main/java/com/jupitters/JupittersHotel/service/impl/BookingServiceImpl.java
@@ -1,8 +1,29 @@
 package com.jupitters.JupittersHotel.service.impl;
 
+import com.jupitters.JupittersHotel.dto.Response;
+import com.jupitters.JupittersHotel.model.Booking;
 import com.jupitters.JupittersHotel.service.BookingService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookingServiceImpl implements BookingService {
+    @Override
+    public Response saveBooking(Long roomId, Long userId, Booking bookingRequest) {
+        return null;
+    }
+
+    @Override
+    public Response findBookingByConfirmationCode(String confirmationCode) {
+        return null;
+    }
+
+    @Override
+    public Response getAllBookings() {
+        return null;
+    }
+
+    @Override
+    public Response cancelBooking(Long bookingId) {
+        return null;
+    }
 }
