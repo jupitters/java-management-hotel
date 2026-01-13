d971dfe7b1e63043267e96986cf584071bb90d5a
jupitters <sampaiobale@gmail.com>
2026-01-13 16:57:53 -0300

    footer component

--- jupitters-management-hotel/src/component/common/Footer.jsx
+++ jupitters-management-hotel/src/component/common/Footer.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+        <span>
+            Hotel | All Rights Reserved &copy; {new Date().getFullYear}
+        </span>
+    </footer>
   )
 }
 
