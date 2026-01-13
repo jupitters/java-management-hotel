ed5937e7fefc0c6aa98ddc16d6f45aed9cc691a5
jupitters <sampaiobale@gmail.com>
2026-01-13 16:46:29 -0300

    navbar

--- jupitters-management-hotel/src/component/common/Navbar.jsx
+++ jupitters-management-hotel/src/component/common/Navbar.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import ApiService from '../../service/ApiService'
 
 const Navbar = () => {
+    const isAuthenticated = ApiService.isAuthenticated();
+    const isAdmin = ApiService.isAdmin();
+    const isUser = ApiService.isUser();
+
   return (
     <div>Navbar</div>
   )
