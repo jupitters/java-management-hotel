b721b4fb90fc3e50d91fff4d79be39a385a95648
jupitters <sampaiobale@gmail.com>
2026-01-13 16:53:22 -0300

    navbar

--- jupitters-management-hotel/src/component/common/Navbar.jsx
+++ jupitters-management-hotel/src/component/common/Navbar.jsx
@@ -12,16 +12,16 @@ const Navbar = () => {
         <div className='navbar-brand'>
             <NavLink to="/home"> Hotel</NavLink>
         </div>
-        <ul>
+        <ul className='navbar-ul'>
             <li><NavLink to="/home" activeClass="active"> Home</NavLink></li>
             <li><NavLink to="/rooms" activeClass="active"> Rooms</NavLink></li>
             <li><NavLink to="/find-booking" activeClass="active"> Find My Bookings</NavLink></li>
 
-            <li><NavLink to="/profile" activeClass="active"> Profile</NavLink></li>
-            <li><NavLink to="/admin" activeClass="active"> Admin</NavLink></li>
+            { isUser && <li><NavLink to="/profile" activeClass="active"> Profile</NavLink></li> }
+            { isAdmin && <li><NavLink to="/admin" activeClass="active"> Admin</NavLink></li> }
             
-            <li><NavLink to="/login" activeClass="active"> Login</NavLink></li>
-            <li><NavLink to="/register" activeClass="active"> Register</NavLink></li>
+            { !isAuthenticated && <li><NavLink to="/login" activeClass="active"> Login</NavLink></li>}
+            { isAuthenticated && <li><NavLink to="/register" activeClass="active"> Register</NavLink></li>}
 
             <li> Logout</li>
         </ul>
