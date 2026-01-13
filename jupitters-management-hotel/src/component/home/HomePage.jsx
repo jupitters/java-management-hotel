f557388166401ec1839424d0b69118ac6de44227
jupitters <sampaiobale@gmail.com>
2026-01-13 17:04:21 -0300

    home page

--- jupitters-management-hotel/src/component/home/HomePage.jsx
+++ jupitters-management-hotel/src/component/home/HomePage.jsx
@@ -16,9 +16,6 @@ const HomePage = () => {
             </header>
         </section>
 
-        <RoomSearch handleSearchResult={handleSearchResult} />
-        <RoomResult roomSearchResults={roomSearchResults} />
-
         <h4><a className="view-rooms-home" href="/rooms">All Rooms</a></h4>
 
         <h2 className="home-services">Services at <span className="phegon-color">Phegon Hotel</span></h2>
