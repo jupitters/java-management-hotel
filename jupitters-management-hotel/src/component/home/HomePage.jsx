0a2eaa573de8add95ee0a91fc3726efa5bb8e464
jupitters <sampaiobale@gmail.com>
2026-01-13 17:02:30 -0300

    home page

--- jupitters-management-hotel/src/component/home/HomePage.jsx
+++ jupitters-management-hotel/src/component/home/HomePage.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div>
+        <section>
+            <header className="header-banner">
+                <img src="./assets/images/hotel.webp" alt="Phegon Hotel" className="header-image" />
+                <div className="overlay"></div>
+                <div className="animated-texts overlay-content">
+                    <h1>
+                        Welcome to <span className="phegon-color">Phegon Hotel</span>
+                    </h1><br />
+                    <h3>Step into a haven of comfort and care</h3>
+                </div>
+            </header>
+        </section>
+    </div>
   )
 }
 
