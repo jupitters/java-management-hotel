ba122410820166d598146610b74d0b298f98a3b0
jupitters <sampaiobale@gmail.com>
2026-01-14 20:44:57 -0300

    app

--- jupitters-management-hotel/src/App.js
+++ jupitters-management-hotel/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './component/common/Navbar'/;
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
