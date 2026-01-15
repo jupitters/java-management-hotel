5c9eeb015c23352c885e2374bff1d5c21b54dc4f
jupitters <sampaiobale@gmail.com>
2026-01-15 15:07:03 -0300

    room result

--- jupitters-management-hotel/src/component/common/RoomResult.jsx
+++ jupitters-management-hotel/src/component/common/RoomResult.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import ApiService from '../../service/ApiService';
 
-const RoomResult = () => {
+const RoomResult = ({ roomSearchResults }) => {
+    const navigate = useNavigate();
+    const isAdmin = ApiService.isAdmin();
   return (
-    <div>RoomResult</div>
+    <section className='room-results'>
+        {roomSearchResults && roomSearchResults.length > 0 && (
+            <div className='room-list'>
+                {roomSearchResults.map((room) => (
+                    <div key={room.id} className='room-list-item'>
+                        <img className='room-list-item-image' src={room.roomPhotoUrl} alt={room.roomType} />
+                            <div className="room-details">
+                                <h3>{room.roomType}</h3>
+                                <p>Price: ${room.roomPrice} / night</p>
+                                <p>Description: {room.roomDescription}</p>
+                            </div>
+                    </div>
+                ))}
+            </div>
+        )}
+    </section>
   )
 }
 
