96a668d9cfce2fd759c212a4928e7e478f213eaf
jupitters <sampaiobale@gmail.com>
2026-01-15 14:37:40 -0300

    room search

--- jupitters-management-hotel/src/component/common/RoomSearch.jsx
+++ jupitters-management-hotel/src/component/common/RoomSearch.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import ApiService from '../../service/ApiService';
 
 const RoomSearch = ({ handleSearchResult }) => {
     const [startDate, setStartDate] = useState(null);
@@ -6,6 +7,18 @@ const RoomSearch = ({ handleSearchResult }) => {
     const [roomType, setRoomType] = useState('');
     const [roomTypes, setRoomTypes] = useState([]);
     const [error, setError] = useState('');
+
+    useEffect(() => {
+        const fetchRoomTypes = async () => {
+            try{
+                const types = await ApiService.getRoomTypes();
+                setRoomTypes(types);
+            }catch(err){
+                console.error(err.message);
+            }
+        }
+        fetchRoomTypes(); 
+    }, [])
 
   return (
     <div>RoomSearch</div>
