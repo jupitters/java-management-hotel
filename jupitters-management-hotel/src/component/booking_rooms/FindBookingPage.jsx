ab88beb50db704fab8958ec8f50fb79c25363c75
jupitters <sampaiobale@gmail.com>
2026-01-16 19:32:21 -0300

    find booking page componnet

--- jupitters-management-hotel/src/component/booking_rooms/FindBookingPage.jsx
+++ jupitters-management-hotel/src/component/booking_rooms/FindBookingPage.jsx
@@ -1,10 +1,27 @@
 import React, { useState } from 'react'
+import ApiService from '../../service/ApiService';
 
 const FindBookingPage = () => {
     const [confirmationCode, setConfirmationCode] = useState('');
     const [bookingDetails, setBookingDetails] = useState(null);
     const [error, setError] = useState(null);
 
+    const handleSearch = async () => {
+        if (!confirmationCode.trim()) {
+            setError("Please Enter a booking confirmation code");
+            setTimeout(() => setError(''), 5000);
+            return;
+        }
+        try {
+            // Call API to get booking details
+            const response = await ApiService.getBookingByConfirmationCode(confirmationCode);
+            setBookingDetails(response.booking);
+            setError(null); // Clear error if successful
+        } catch (error) {
+            setError(error.response?.data?.message || error.message);
+            setTimeout(() => setError(''), 5000);
+        }
+    };
     
   return (
     <div>FindBookingPage</div>
