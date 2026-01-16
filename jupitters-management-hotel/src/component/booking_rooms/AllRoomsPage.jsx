047bfc16548383d4e014bf0ae54a56178c606cf8
jupitters <sampaiobale@gmail.com>
2026-01-16 19:21:22 -0300

    all rooms page component

--- jupitters-management-hotel/src/component/booking_rooms/AllRoomsPage.jsx
+++ jupitters-management-hotel/src/component/booking_rooms/AllRoomsPage.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const AllRoomsPage = () => {
+    const [rooms, setRooms] = useState([]);
+    const [filteredRooms, setFilteredRooms] = useState([]);
+    const [roomTypes, setRoomTypes] = useState([]);
+    const [selectedRoomType, setSelectedRoomType] = useState('');
+    const [currentPage, setCurrentPage] = useState(1);
+    const [roomsPerPage] = useState(5);
+
   return (
     <div>AllRoomsPage</div>
   )
