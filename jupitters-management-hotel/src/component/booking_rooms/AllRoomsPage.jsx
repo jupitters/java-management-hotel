6c02bf37aa5a22ea9c5789042a03bf5442e87d86
jupitters <sampaiobale@gmail.com>
2026-01-16 19:23:43 -0300

    all rooms page component

--- jupitters-management-hotel/src/component/booking_rooms/AllRoomsPage.jsx
+++ jupitters-management-hotel/src/component/booking_rooms/AllRoomsPage.jsx
@@ -40,6 +40,21 @@ const AllRoomsPage = () => {
     fetchRoomTypes();
   }, []);
 
+  const handleRoomTypeChange = (e) => {
+    setSelectedRoomType(e.target.value);
+    filterRooms(e.target.value);
+  };
+
+  const filterRooms = (type) => {
+    if (type === '') {
+      setFilteredRooms(rooms);
+    } else {
+      const filtered = rooms.filter((room) => room.roomType === type);
+      setFilteredRooms(filtered);
+    }
+    setCurrentPage(1); // Reset to first page after filtering
+  };
+
   return (
     <div>AllRoomsPage</div>
   )
