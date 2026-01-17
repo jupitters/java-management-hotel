e57abba6c39064605770ea857ee710b921becefc
jupitters <sampaiobale@gmail.com>
2026-01-17 17:20:32 -0300

    room details page component

--- jupitters-management-hotel/src/component/booking_rooms/RoomDetailsPage.jsx
+++ jupitters-management-hotel/src/component/booking_rooms/RoomDetailsPage.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
 
 const RoomDetailsPage = () => {
+  const navigate = useNavigate();
+  const { roomId } = useParams();
+  const [roomDetails, setRoomDetails] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [checkInDate, setCheckInDate] = useState(null);
+  const [checkOutDate, setCheckOutDate] = useState(null);
+  const [numAdults, setNumAdults] = useState(1);
+  const [numChildren, setNumChildren] = useState(0); 
+  const [totalPrice, setTotalPrice] = useState(0); 
+  const [totalGuests, setTotalGuests] = useState(1); 
+  const [showDatePicker, setShowDatePicker] = useState(false); 
+  const [userId, setUserId] = useState('');
+  const [showMessage, setShowMessage] = useState(false); 
+  const [confirmationCode, setConfirmationCode] = useState(''); 
+  const [errorMessage, setErrorMessage] = useState('');
   return (
     <div>RoomDetailsPage</div>
   )
