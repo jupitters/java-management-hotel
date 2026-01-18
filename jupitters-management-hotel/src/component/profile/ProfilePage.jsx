8bd3c9ebba80e50dee3db34556fec71d1b7d20c8
jupitters <sampaiobale@gmail.com>
2026-01-18 18:16:54 -0300

    profile page component

--- jupitters-management-hotel/src/component/profile/ProfilePage.jsx
+++ jupitters-management-hotel/src/component/profile/ProfilePage.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const ProfilePage = () => {
+    const [user, setUser] = useState(null);
+    const [error, setError] = useState(null);
+    const navigate = useNavigate();
+
   return (
     <div>ProfilePage</div>
   )
