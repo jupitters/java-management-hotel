2ea7be08a20189d3a7fa131e2c06b367b3958f2f
jupitters <sampaiobale@gmail.com>
2026-01-18 18:04:21 -0300

    room details page component route

--- jupitters-management-hotel/src/App.js
+++ jupitters-management-hotel/src/App.js
@@ -5,6 +5,7 @@ import HomePage from './component/home/HomePage';
 import Footer from './component/common/Footer';
 import AllRoomsPage from './component/booking_rooms/AllRoomsPage';
 import FindBookingPage from './component/booking_rooms/FindBookingPage';
+import RoomDetailsPage from './component/booking_rooms/RoomDetailsPage';
 
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
             <Route exact path='/home' element={<HomePage />} />
             <Route exact path='/rooms' element={<AllRoomsPage />} />
             <Route path='/find-booking' element={<FindBookingPage />} />
+            <Route path='/room-details/:roomId' element={<RoomDetailsPage />} />
           </Routes>
         </div>
         <Footer />
