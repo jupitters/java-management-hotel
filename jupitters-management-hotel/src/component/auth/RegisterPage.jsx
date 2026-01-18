1b989000fb6d9a4b1bf673dfd4c147168d2ae87a
jupitters <sampaiobale@gmail.com>
2026-01-18 18:08:55 -0300

    register page

--- jupitters-management-hotel/src/component/auth/RegisterPage.jsx
+++ jupitters-management-hotel/src/component/auth/RegisterPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
+import ApiService from '../../service/ApiService';
 
 const RegisterPage = () => {
     const navigate = useNavigate();
@@ -25,6 +26,36 @@ const RegisterPage = () => {
             return false;
         }
         return true;
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (!validateForm()) {
+            setErrorMessage('Please fill all the fields.');
+            setTimeout(() => setErrorMessage(''), 5000);
+            return;
+        }
+        try {
+            const response = await ApiService.registerUser(formData);
+
+            if (response.statusCode === 200) {
+                setFormData({
+                    name: '',
+                    email: '',
+                    password: '',
+                    phoneNumber: ''
+                });
+                setSuccessMessage('User registered successfully');
+                setTimeout(() => {
+                    setSuccessMessage('');
+                    navigate('/');
+                }, 3000);
+            }
+        }
+         catch (error) {
+            setErrorMessage(error.response?.data?.message || error.message);
+            setTimeout(() => setErrorMessage(''), 5000);
+        }
     };
 
 
