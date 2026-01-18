f33388ba5ba0f62f770fed649f3b8866de1b30b1
jupitters <sampaiobale@gmail.com>
2026-01-18 18:17:10 -0300

    profile page component

--- jupitters-management-hotel/src/component/profile/ProfilePage.jsx
+++ jupitters-management-hotel/src/component/profile/ProfilePage.jsx
@@ -1,10 +1,27 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
+import ApiService from '../../service/ApiService';
 
 const ProfilePage = () => {
     const [user, setUser] = useState(null);
     const [error, setError] = useState(null);
     const navigate = useNavigate();
+
+    useEffect(() => {
+        const fetchUserProfile = async () => {
+            try {
+                const response = await ApiService.getUserProfile();
+                // Fetch user bookings using the fetched user ID
+                const userPlusBookings = await ApiService.getUserBookings(response.user.id);
+                setUser(userPlusBookings.user)
+
+            } catch (error) {
+                setError(error.response?.data?.message || error.message);
+            }
+        };
+
+        fetchUserProfile();
+    }, []);
 
   return (
     <div>ProfilePage</div>
