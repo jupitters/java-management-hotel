c6b79af8a4ebe8bab371fbcffb94c8a4406a140d
jupitters <sampaiobale@gmail.com>
2026-01-18 18:06:38 -0300

    register page

--- jupitters-management-hotel/src/component/auth/RegisterPage.jsx
+++ jupitters-management-hotel/src/component/auth/RegisterPage.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const RegisterPage = () => {
+    const navigate = useNavigate();
+
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        phoneNumber: ''
+    });
+
+    const [errorMessage, setErrorMessage] = useState('');
+    const [successMessage, setSuccessMessage] = useState('');
+
+    
   return (
     <div>RegisterPage</div>
   )
