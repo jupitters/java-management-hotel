8f25f7f8cc88c0b4cad4a690175ac3649c0b2717
jupitters <sampaiobale@gmail.com>
2026-01-18 18:10:03 -0300

    login page

--- jupitters-management-hotel/src/component/auth/LoginPage.jsx
+++ jupitters-management-hotel/src/component/auth/LoginPage.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const LoginPage = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const from = location.state?.from?.pathname || '/home';
+
   return (
     <div>LoginPage</div>
   )
