0784e6966668e5e7901bf87c0fe4b851eb266f28
jupitters <sampaiobale@gmail.com>
2026-01-18 18:10:21 -0300

    login page

--- jupitters-management-hotel/src/component/auth/LoginPage.jsx
+++ jupitters-management-hotel/src/component/auth/LoginPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom';
+import ApiService from '../../service/ApiService';
 
 const LoginPage = () => {
     const [email, setEmail] = useState('');
@@ -9,6 +10,28 @@ const LoginPage = () => {
     const location = useLocation();
 
     const from = location.state?.from?.pathname || '/home';
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if (!email || !password) {
+            setError('Please fill in all fields.');
+            setTimeout(() => setError(''), 5000);
+            return;
+        }
+
+        try {
+            const response = await ApiService.loginUser({email, password});
+            if (response.statusCode === 200) {
+                localStorage.setItem('token', response.token);
+                localStorage.setItem('role', response.role);
+                navigate(from, { replace: true });
+            }
+        } catch (error) {
+            setError(error.response?.data?.message || error.message);
+            setTimeout(() => setError(''), 5000);
+        }
+    };
 
   return (
     <div>LoginPage</div>
