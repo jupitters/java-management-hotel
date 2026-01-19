8e82e1d88ad75634094ee25c9bca185a733beb0b
jupitters <sampaiobale@gmail.com>
2026-01-19 15:44:09 -0300

    manage bookings page

---
+++ jupitters-management-hotel/src/component/admin/AddRoomPage.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react'
+import ApiService from '../../service/ApiService';
+
+const AddRoomPage = () => {
+    const navigate = useNavigate();
+    const [roomDetails, setRoomDetails] = useState({
+        roomPhotoUrl: '',
+        roomType: '',
+        roomPrice: '',
+        roomDescription: '',
+    });
+    const [file, setFile] = useState(null);
+    const [preview, setPreview] = useState(null);
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState('');
+    const [roomTypes, setRoomTypes] = useState([]);
+    const [newRoomType, setNewRoomType] = useState(false);
+
+
+    useEffect(() => {
+        const fetchRoomTypes = async () => {
+            try {
+                const types = await ApiService.getRoomTypes();
+                setRoomTypes(types);
+            } catch (error) {
+                console.error('Error fetching room types:', error.message);
+            }
+        };
+        fetchRoomTypes();
+    }, []);
+
+
+
+  return (
+    <div>AddRoomPage</div>
+  )
+}
+
+export default AddRoomPage
