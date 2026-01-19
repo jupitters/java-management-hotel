ef70e124ea632539a332c51a33ee3c4ccfa808a7
jupitters <sampaiobale@gmail.com>
2026-01-19 15:55:16 -0300

    admin routes

--- jupitters-management-hotel/src/component/admin/EditBookingPage.jsx
+++ jupitters-management-hotel/src/component/admin/EditBookingPage.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom';
+import ApiService from '../../service/ApiService';
 
 const EditBookingPage = () => {
     const navigate = useNavigate();
@@ -20,6 +21,27 @@ const EditBookingPage = () => {
 
         fetchBookingDetails();
     }, [bookingCode]);
+
+     const acheiveBooking = async (bookingId) => {
+        if (!window.confirm('Are you sure you want to Acheive this booking?')) {
+            return; // Do nothing if the user cancels
+        }
+
+        try {
+            const response = await ApiService.cancelBooking(bookingId);
+            if (response.statusCode === 200) {
+                setSuccessMessage("The boking was Successfully Acheived")
+                
+                setTimeout(() => {
+                    setSuccessMessage('');
+                    navigate('/admin/manage-bookings');
+                }, 3000);
+            }
+        } catch (error) {
+            setError(error.response?.data?.message || error.message);
+            setTimeout(() => setError(''), 5000);
+        }
+    };
 
   return (
     <div>EditBookingPage</div>
