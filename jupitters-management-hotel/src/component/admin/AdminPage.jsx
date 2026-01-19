251863ee09fdf4a86a83d7d2cd9dbf5a8493b5a8
jupitters <sampaiobale@gmail.com>
2026-01-19 15:31:35 -0300

    admin page

--- jupitters-management-hotel/src/component/admin/AdminPage.jsx
+++ jupitters-management-hotel/src/component/admin/AdminPage.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import ApiService from '../../service/ApiService';
 
 const AdminPage = () => {
+    const [adminName, setAdminName] = useState('');
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const fetchAdminName = async () => {
+            try {
+                const response = await ApiService.getUserProfile();
+                setAdminName(response.user.name);
+            } catch (error) {
+                console.error('Error fetching admin details:', error.message);
+            }
+        };
+
+        fetchAdminName();
+    }, []);
+
   return (
     <div>AdminPage</div>
   )
