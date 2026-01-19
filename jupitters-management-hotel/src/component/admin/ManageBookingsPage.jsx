294491e1cb869b1b67320dbc9ce038f09cf8ec90
jupitters <sampaiobale@gmail.com>
2026-01-19 15:41:39 -0300

    manage bookings page

---
+++ jupitters-management-hotel/src/component/admin/ManageBookingsPage.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import ApiService from '../../service/ApiService';
+
+const ManageBookingsPage = () => {
+    const [bookings, setBookings] = useState([]);
+    const [filteredBookings, setFilteredBookings] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [currentPage, setCurrentPage] = useState(1);
+    const [bookingsPerPage] = useState(6);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const fetchBookings = async () => {
+            try {
+                const response = await ApiService.getAllBookings();
+                const allBookings = response.bookingList;
+                setBookings(allBookings);
+                setFilteredBookings(allBookings);
+            } catch (error) {
+                console.error('Error fetching bookings:', error.message);
+            }
+        };
+
+        fetchBookings();
+    }, []);
+
+  return (
+    <div>ManageBookingsPage</div>
+  )
+}
+
+export default ManageBookingsPage
