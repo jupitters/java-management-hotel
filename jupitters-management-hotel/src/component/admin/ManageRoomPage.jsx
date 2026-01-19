73395806fdeefd84fa6082df428b6310add20e61
jupitters <sampaiobale@gmail.com>
2026-01-19 15:37:59 -0300

    admin page

---
+++ jupitters-management-hotel/src/component/admin/ManageRoomPage.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import ApiService from '../../service/ApiService';
+
+const ManageRoomPage = () => {
+    const [rooms, setRooms] = useState([]);
+    const [filteredRooms, setFilteredRooms] = useState([]);
+    const [roomTypes, setRoomTypes] = useState([]);
+    const [selectedRoomType, setSelectedRoomType] = useState('');
+    const [currentPage, setCurrentPage] = useState(1);
+    const [roomsPerPage] = useState(5);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+    const fetchRooms = async () => {
+      try {
+        const response = await ApiService.getAllRooms();
+        const allRooms = response.roomList;
+        setRooms(allRooms);
+        setFilteredRooms(allRooms);
+      } catch (error) {
+        console.error('Error fetching rooms:', error.message);
+      }
+    };
+
+    const fetchRoomTypes = async () => {
+      try {
+        const types = await ApiService.getRoomTypes();
+        setRoomTypes(types);
+      } catch (error) {
+        console.error('Error fetching room types:', error.message);
+      }
+    };
+
+    fetchRooms();
+    fetchRoomTypes();
+  }, []);
+    
+  return (
+    <div>ManageRoomPage</div>
+  )
+}
+
+export default ManageRoomPage
